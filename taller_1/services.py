fa291c4ae725c0d97585980a5a9dd6c0bf19796e
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-03-03 16:53:22 -0500

    Get artists

---
+++ taller_1/services.py
@@ -0,0 +1,7 @@
+import pandas as pd, numpy as np, time, os
+
+def get_possible_artists():
+    return {'artists': [
+		{'id': 1, 'name': 'Artist 1'},
+		{'id': 2, 'name': 'Artist 2'}
+		]}
--- taller_1/views.py
+++ taller_1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from os.path import exists
+from taller_1.services import get_possible_artists
 
 def index(request):
-	return render(request, 'taller_1/taller_1.html')
+	return render(request, 'taller_1/taller_1.html', get_possible_artists())
